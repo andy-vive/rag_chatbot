e85d42e1e38158ef0b07edef308dc2741f9ce0b8
andy-duypham <duy.pt@wearecare.sg>
2025-10-29 08:32:21 +0700

    feat: using langchain

---
+++ agent/core/agent.py
@@ -0,0 +1,7 @@
+
+class RAGAgent:
+    def __init__(self):
+        pass
+
+    async def chat(self, query: str, session_id: str) -> any:
+        pass
---
+++ agent/core/llm.py
@@ -0,0 +1,29 @@
+from ast import Tuple
+
+from langchain_core.embeddings import Embeddings
+from langchain_core.language_models import BaseChatModel
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+
+from config import Setting
+
+
+class LLMFactory:
+
+    @staticmethod
+    def create(setting: Setting) -> Tuple[BaseChatModel, Embeddings]:
+        if setting.llm_type == "google":
+            llm = ChatGoogleGenerativeAI(
+                google_api_key=setting.google_api_key,
+                model=setting.gemini_model,
+                max_tokens=setting.max_tokens,
+                temperature=0.7,
+            )
+
+            embedding = GoogleGenerativeAIEmbeddings(
+                google_api_key=setting.google_api_key,
+                model=setting.embedding_model,
+            )
+
+            return llm, embedding
+        else:
+            raise ValueError(f"Unknown LLM type: {setting.llm_type}")
